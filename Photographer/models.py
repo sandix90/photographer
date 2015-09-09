5d117a82e9727a3ed4be589d03214f61b6def69f
sandix <alex@sandix.org>
2015-09-09 09:49:06 +0500

    First commit

---
+++ Photographer/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django import forms
+from froala_editor.fields import FroalaField
+
+class Test(models.Model):
+    title = forms.CharField(max_length=100)
+    froala = FroalaField()
---
+++ Photographer/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render_to_response
+from Photographer.models import Test
+
+def index(request):
+    objects = Test.objects.all()
+
+    return render_to_response('Photographer\index.html', {'objects':objects})
+
